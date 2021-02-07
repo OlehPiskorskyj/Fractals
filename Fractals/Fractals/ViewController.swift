19b9e6f38cec5820b114ce9a4e8e2416a571f9a8
Oleh Piskorskyj <oleh.piskorskyj@gmail.com>
2021-02-07 01:17:56 +0100

    - init of project

---
+++ Fractals/Fractals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fractals
+//
+//  Created by Oleh Piskorskyj on 07/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
