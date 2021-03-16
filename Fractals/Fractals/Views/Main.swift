bf1a1f7a3c1d4cfd1cdca8272b85e7240b6aaca7
Oleh Piskorskyj <oleh.piskorskyj@gmail.com>
2021-03-16 21:07:25 +0100

    - refactoring. Add segues to Main screen.

--- Fractals/Fractals/Views/Main.swift
+++ Fractals/Fractals/Views/Main.swift
@@ -22,20 +22,4 @@ class Main: UIViewController {
         btnJulia.defaultTouchesEnabled = true
         btnTree.defaultTouchesEnabled = true
     }
-    
-    // MARK: - events
-    @IBAction func btnMandelbrotClick(_ sender: Any) {
-        let mandelbrot = self.storyboard!.instantiateViewController(withIdentifier: "MandelbrotSceen") as! MandelbrotSceen
-        self.navigationController!.pushViewController(mandelbrot, animated: true)
-    }
-    
-    @IBAction func btnJuliaClick(_ sender: Any) {
-        let julia = self.storyboard!.instantiateViewController(withIdentifier: "JuliaScreen") as! JuliaScreen
-        self.navigationController!.pushViewController(julia, animated: true)
-    }
-    
-    @IBAction func btnTreeClick(_ sender: Any) {
-        let tree = self.storyboard!.instantiateViewController(withIdentifier: "TreeScreen") as! TreeScreen
-        self.navigationController!.pushViewController(tree, animated: true)
-    }
 }
